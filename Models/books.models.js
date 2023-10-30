bd91bd57f18dde04399f738cdedef58341fbf14f
Divyansh <divyanshgoel777@gmail.com>
2023-10-30 12:21:29 +0530

    Model Updated

--- Models/books.models.js
+++ Models/books.models.js
@@ -3,7 +3,8 @@ const mongoose=require("mongoose");
 const bookschema=mongoose.Schema({
     title:{type:String,required:true},
     author:{type:String,required:true},
-    summary:{type:String,required:true}
+    summary:{type:String,required:true},
+    createdAt:{type:Date,required:true}
 },{
     versionKey:false
 }
