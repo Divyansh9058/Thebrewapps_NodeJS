02a676bd1c141b41f15d72d4b3ce9d9c8f9c748c
Divyansh <divyanshgoel777@gmail.com>
2023-10-30 12:34:00 +0530

    Update Code

--- Routes/books.routes.js
+++ Routes/books.routes.js
@@ -1,72 +1,89 @@
-const express=require("express");
-const app=express();
-const bookRouter=express.Router();
-const {bookmodel}=require("../Models/books.models")
+const express = require("express");
+const app = express();
+const bookRouter = express.Router();
+const { bookmodel } = require("../Models/books.models")
 
 app.use(express.json());
 
-bookRouter.get("/all",async(req,res)=>{
-
-    try{
-      const notes= await bookmodel.find();
-        res.status(200).send(notes)
-    }
-    catch (error) {
-        res.status(500).send({msg:"Something went wrong",error:error.message})
-    }
-        
-});
-
-
-bookRouter.post("/create",async(req,res)=>{
+// Create or Add a new book.
+bookRouter.post("/create", async (req, res) => {
     try {
-        const payload=req.body;
-        const newbook= await bookmodel(payload);
+        const payload = req.body;
+        const newbook = await bookmodel(payload);
         newbook.save();
         res.status(200).send("New book data recorded")
     } catch (error) {
-        res.status(500).send({msg:"Something went wrong",error:error.message})
+        res.status(500).send({ msg: "Something went wrong", error: error.message })
     }
 });
 
-
-bookRouter.patch("/update/:id",async(req,res)=>{
+// Fetch all data
+bookRouter.get("/all", async (req, res) => {
     try {
-        const id=req.params.id;
-        const payload=req.body;
-        const data = await bookmodel.findByIdAndUpdate(id,payload);
+        const notes = await bookmodel.find();
+        res.status(200).send(notes)
+    }
+    catch (error) {
+        res.status(500).send({ msg: "Something went wrong", error: error.message })
+    }
+});
 
-        if(!data){
-            res.status(400).send({"msg":`Data Not Found`})
-        }else{
-            res.status(200).send({"msg":`note with id:${id} has been updated`})
+// Fetch data by specific id
+bookRouter.get("/find/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await bookmodel.findById(id);
+
+        if (!data) {
+            res.status(404).send({ "msg": `Data with ID ${id} Not Found` });
+        } else {
+            res.status(200).send(data);
         }
 
     } catch (error) {
-        res.status(500).send({msg:"something went wrong",error:error.message})
+        res.status(500).send({ msg: "Something went wrong", error: error.message });
     }
 });
 
 
+// Update data by specific id
+bookRouter.patch("/update/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const payload = req.body;
+        const data = await bookmodel.findByIdAndUpdate(id, payload);
 
-bookRouter.delete('/delete/:id',async(req,res)=>{
-    try{
-        const bookId=req.params.id;
-        const data = await bookmodel.findByIdAndDelete({_id:bookId})
+        if (!data) {
+            res.status(404).send({ "msg": `Data Not Found` })
+        } else {
+            res.status(200).send({ "msg": `note with id:${id} has been updated` })
+        }
 
-        if(data){
-            res.status(200).send({"msg":`note with id:${bookId} has been deleted`});
-        }else{
-            res.status(400).send({"msg":`Data Not Found`})
+    } catch (error) {
+        res.status(500).send({ msg: "something went wrong", error: error.message })
+    }
+});
+
+
+// Delete data by specific id
+bookRouter.delete('/delete/:id', async (req, res) => {
+    try {
+        const bookId = req.params.id;
+        const data = await bookmodel.findByIdAndDelete({ _id: bookId })
+
+        if (data) {
+            res.status(200).send({ "msg": `note with id:${bookId} has been deleted` });
+        } else {
+            res.status(400).send({ "msg": `Data Not Found` })
         }
 
     }
     catch (error) {
-        res.status(500).send({msg:"Something went wrong",error:error.message})
+        res.status(500).send({ msg: "Something went wrong", error: error.message })
     }
 });
 
 
-module.exports={
+module.exports = {
     bookRouter
 }
